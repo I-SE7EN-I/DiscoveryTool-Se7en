c2bb6cfbcbb64b06fe63e98bb7b6dba06e31c7e2
I-SE7EN-I <austin_caudill@msn.com>
2025-02-21 10:51:58 -0500

    Add files via upload

---
+++ Run.bat
@@ -0,0 +1,5 @@
+@echo off
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "DiscoveryUpdate.ps1"
+
+::Option to run the tool directly without checking for updates.
+::PowerShell -NoProfile -ExecutionPolicy Bypass -File "DiscoveryTool-Se7en.ps1"
